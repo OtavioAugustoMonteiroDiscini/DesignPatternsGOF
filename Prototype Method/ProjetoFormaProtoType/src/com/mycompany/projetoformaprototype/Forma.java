b578b4c774696dba0bb4f1f6ee7b312544fb1920
Otavio Augusto Monteiro Discini <142923525+OtavioAugustoMonteiroDiscini@users.noreply.github.com>
2025-05-09 14:22:49 -0300

    Delete Prototype Method/ProjetoFormaProtoType directory

--- Prototype Method/ProjetoFormaProtoType/src/com/mycompany/projetoformaprototype/Forma.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.projetoformaprototype;
-
-
-//extends cloneable diz que todos os objetos dessa interface pode ser clonado
-public interface Forma extends Cloneable{
-
-    Forma clonar();
-
-}
